cef5b9603a62c89aa7ab3488ea989a6835409463
Leonardo Reis <leonardomfreis@gmail.com>
2019-09-27 12:29:38 -0300

    inital

--- p02/datatypes.h
+++ p02/datatypes.h
@@ -4,36 +4,37 @@
 //
 // Estruturas de dados internas do sistema operacional
 
+#include <ucontext.h>
+
 #ifndef __DATATYPES__
 #define __DATATYPES__
 
 // Estrutura que define uma tarefa
-typedef struct task_t
-{
-  // preencher quando necessário
+typedef struct task_t {
+
+  int id;                   //id de uma tarefa
+  ucontext_t task_ctx;      //contexto de uma tarefa
+  ucontext_t main_ctx;      //contexto da main
+
 } task_t ;
 
 // estrutura que define um semáforo
-typedef struct
-{
+typedef struct {
   // preencher quando necessário
 } semaphore_t ;
 
 // estrutura que define um mutex
-typedef struct
-{
+typedef struct {
   // preencher quando necessário
 } mutex_t ;
 
 // estrutura que define uma barreira
-typedef struct
-{
+typedef struct {
   // preencher quando necessário
 } barrier_t ;
 
 // estrutura que define uma fila de mensagens
-typedef struct
-{
+typedef struct {
   // preencher quando necessário
 } mqueue_t ;
 
