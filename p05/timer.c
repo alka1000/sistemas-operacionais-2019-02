1151795281af847b5b68b77945ca0056933fda16
leonardo <leonardomfreis@gmail.com>
2019-09-29 20:52:42 -0300

    finalizado o 5

--- p05/timer.c
+++ p05/timer.c
@@ -18,12 +18,9 @@ struct sigaction action ;
 struct itimerval timer;
 
 // tratador do sinal
-void tratador (int signum)
-{
-  printf ("Recebi o sinal %d\n", signum) ;
-}
 
-int main ()
+
+int start_tim (void *tratador)
 {
   // registra a a��o para o sinal de timer SIGALRM
   action.sa_handler = tratador ;
@@ -36,10 +33,10 @@ int main ()
   }
 
   // ajusta valores do temporizador
-  timer.it_value.tv_usec = 0 ;      // primeiro disparo, em micro-segundos
-  timer.it_value.tv_sec  = 3 ;      // primeiro disparo, em segundos
-  timer.it_interval.tv_usec = 0 ;   // disparos subsequentes, em micro-segundos
-  timer.it_interval.tv_sec  = 1 ;   // disparos subsequentes, em segundos
+  timer.it_value.tv_usec = 30 ;      // primeiro disparo, em micro-segundos
+  //timer.it_value.tv_usec  = 1 ;      // primeiro disparo, em segundos
+  timer.it_interval.tv_usec = 200 ;   // disparos subsequentes, em micro-segundos
+  //timer.it_interval.tv_usec  = 0 ;   // disparos subsequentes, em segundos
 
   // arma o temporizador ITIMER_REAL (vide man setitimer)
   if (setitimer (ITIMER_REAL, &timer, 0) < 0)
@@ -49,5 +46,4 @@ int main ()
   }
 
   // laco vazio
-  while (1) ;
 }
---
+++ p05/timer.h
@@ -0,0 +1,7 @@
+
+#include <sys/time.h>
+#include <signal.h>
+
+
+
+void init_timer();
